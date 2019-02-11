859fdaab066d2195374a899991c56aefa04b5ec2
Brad Margerrison <bdm770@gmail.com>
2019-02-11 19:01:29 +0000

    Classic FizzBuzz app in C#

---
+++ C#/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main()
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("{0} {1}{2}", i,
+                    Convert.ToBoolean(i % 3) ? "" : "Fizz", Convert.ToBoolean(i % 5) ? "" : "Buzz");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
